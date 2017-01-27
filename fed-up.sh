30f8112e8ee31604d06b80534051501241acc2e6
Maru Newby <marun@redhat.com>
2017-01-26 21:46:55 -0800

    Add dns config patching to fed-up.sh

--- fed-up.sh
+++ fed-up.sh
@@ -5,28 +5,21 @@ set -o nounset
 set -o pipefail
 
 NAMESPACE="federation-system"
+KC="kubectl --namespace=${NAMESPACE}"
 NAME="echelon"
-AUTH_FILE="$(dirname "${BASH_SOURCE}/auth.json")"
+AUTH_FILE="$(dirname "${BASH_SOURCE}")/auth-creds.json"
 
 if [[ ! -f "${AUTH_FILE}" ]]; then
   echo "Missing auth file ${AUTH_FILE}"
   exit 1
 fi
 
-# TODO kubefed should allow deployment into an existing namespace to
-# allow setting the correct permissions for openshift before init is
-# called
+# TODO maybe kubefed should allow deployment into an existing
+# namespace to allow setting the correct permissions for openshift
+# before init is called?
 kubefed init "${NAME}" --dns-zone-name="federation.thesprawl.net."\
- --federation-system-namespace="${NAMESPACE}"
+        --federation-system-namespace="${NAMESPACE}"
 
-KC="kubectl --namespace=${NAMESPACE}"
-
-# DNS credentials are automatically available in the cloud, but it's
-# necessary to manually make then available here.
-${KC} create secret generic auth-creds --from-file="${AUTH_FILE}"
-
-# TODO have to mount the secret volume in the deployment template for
-# the controller manager
 
 ## Need to perform post-init fixup to ensure compatibility with openshift
 
@@ -34,23 +27,41 @@ ${KC} create secret generic auth-creds --from-file="${AUTH_FILE}"
 # the api pod can bind to 443.
 oadm policy add-scc-to-user anyuid "system:serviceaccount:${NAMESPACE}:default"
 
-# Ensure the controller manager will be able to read secrets
-# TODO is there a better role for this?
-oadm policy add-role-to-user cluster-reader system:serviceaccount:federation-system:default
+# Ensure the controller manager will be able to access secrets
+oadm policy add-role-to-user admin system:serviceaccount:"${NAMESPACE}":default
 
 # Remove the alpha annotation on the pvc since it doesn't seem to be
 # compatible with openshift.
 ${KC} annotate pvc "${NAME}-apiserver-etcd-claim" 'volume.alpha.kubernetes.io/storage-class'-
 
-function redeploy() {
-  local pod_name=$1
+# It should now be safe to redeploy the api. The controller manager
+# will be updated separately due to needing to be patched with dns
+# configuration.
+${KC} scale deploy "${NAME}-apiserver" --replicas=0
+${KC} scale deploy "${NAME}-apiserver" --replicas=1
 
-  ${KC} scale deploy "${pod_name}" --replicas=0
-  ${KC} scale deploy "${pod_name}" --replicas=1
-}
 
-# It should now be safe to redeploy the api and controller.
-redeploy "${NAME}-apiserver"
-# Wait for the api server to redeploy before starting the controller manager
-sleep 5
-redeploy "${NAME}-controller-manager"
+## DNS credentials are automatically available in the cloud, but it's
+## necessary to manually make then available here.
+
+SECRET_NAME="auth-creds"
+MOUNT_PATH="/creds"
+QUALIFIED_PATH="/creds/${AUTH_FILE}"
+
+# Add the auth file as a secret
+${KC} create secret generic "${SECRET_NAME}" --from-file="${AUTH_FILE}"
+
+# Scale the deployment down to avoid triggering an update after every patch
+${KC} scale deploy "${NAME}-controller-manager" --replicas=0
+
+# Add volume for the secret
+${KC} patch deploy ${NAME}-controller-manager --type=json -p='[{"op": "add", "path": "/spec/template/spec/volumes/0", "value": {"name": "'"${SECRET_NAME}"'", "secret": {"defaultMode": "420", "secretName": "'"${SECRET_NAME}"'"}}}]'
+
+# Add volume mount
+${KC} patch deploy ${NAME}-controller-manager --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/volumeMounts/0", "value": {"mountPath": "'"${MOUNT_PATH}"'", "name": "'"${SECRET_NAME}"'", "readOnly": true}}]'
+
+# Add an env directive to point to the mounted file
+${KC} patch deploy ${NAME}-controller-manager --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/env/0", "value": {"name": "GOOGLE_APPLICATION_CREDENTIALS", "value": "'"${QUALIFIED_PATH}"'"}}]'
+
+# Scale the deployment back up
+${KC} scale deploy "${NAME}-controller-manager" --replicas=1
