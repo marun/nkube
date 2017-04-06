2149ee986610366b7ea48bae3a34b3b95654e71a
Maru Newby <marun@redhat.com>
2017-04-06 16:24:44 -0700

    Improve fed-up.sh docs and use of variables.

--- tools/fed-up.sh
+++ tools/fed-up.sh
@@ -1,50 +1,70 @@
 #!/bin/bash
 
-# This script automates deployment of a kubernetes federation control
-# plane on an origin cluster.  It's been tested with an origin cluster
-# deployed with the oc-cluster-up.sh script also located in this repo.
+# This script automates deployment of a kubernetes federation control plane on an origin
+# cluster.   It's been tested with a 1.5 origin cluster deployed as follows:
+#
+#    oc cluster up && oc login -u system:admin && oc project default
+#
+# Note: persistence is not possible due to 'kubefed init' deploying a pvc with an
+#       annotation (volume.alpha.kubernetes.io/storage-class) that is not compatible with
+#       openshift.
+#
+# Dependencies: oadm, kubectl, kubefed
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-HYPERKUBE_PATH=/home/dev/src/k8s/src/k8s.io/kubernetes/_output/bin/hyperkube
-NAMESPACE="federation-system"
-KC="kubectl --namespace=${NAMESPACE}"
-NAME="echelon"
+FEDERATION_NAMESPACE="${FEDERATION_NAMESPACE:-federation-system}"
+FEDERATION_NAME="${FEDERATION_NAME:-echelon}"
+HYPERKUBE_PATH="${HYPERKUBE_PATH:-/home/dev/src/k8s/src/k8s.io/kubernetes/_output/bin/hyperkube}"
 
-## Need to perform pre-init fixup to ensure compatibility with openshift
+KC="kubectl --namespace=${FEDERATION_NAMESPACE}"
+CONTROLLER_MANAGER="${FEDERATION_NAME}-controller-manager"
+APISERVER="${FEDERATION_NAME}-apiserver"
+SERVICE_ACCOUNT="system:serviceaccount:${FEDERATION_NAMESPACE}:default"
+
+## Pre-init fixup
 
 # Ensure deployments can create pods that can bind to ports < 1024 so
-# the api pod can bind to 443.
-oadm policy add-scc-to-user anyuid "system:serviceaccount:${NAMESPACE}:default"
+# the federation apiserver pod can bind to 443.
+oadm policy add-scc-to-user anyuid "${SERVICE_ACCOUNT}"
 
-# Ensure the controller manager will be able to access secrets
-oadm policy add-role-to-user admin system:serviceaccount:"${NAMESPACE}":default
+if [[ -f "${HYPERKUBE_PATH}" ]]; then
+  # Ensure the controller manager can mount hyperkube from a hostpath for development
+  oadm policy add-scc-to-user privileged "${SERVICE_ACCOUNT}"
+fi
 
-# Ensure the controller manager can bin mount hyperkube for development
-oadm policy add-scc-to-user privileged system:serviceaccount:"${NAMESPACE}":default
 
-# TODO enable dns configuration
-kubefed init "${NAME}" --dns-provider=google-clouddns --etcd-persistent-storage=false --federation-system-namespace="${NAMESPACE}"
+## Deploy!
+kubefed init "${FEDERATION_NAME}" --dns-provider=google-clouddns \
+        --etcd-persistent-storage=false \
+        --federation-system-namespace="${FEDERATION_NAMESPACE}"
 
-# Remove the alpha annotation on the pvc since it doesn't seem to be
-# compatible with openshift.
-# ${KC} annotate pvc "${NAME}-apiserver-etcd-claim" 'volume.alpha.kubernetes.io/storage-class'-
 
-# # Scale the deployment down to avoid triggering an update after each patch
-${KC} scale deploy "${NAME}-controller-manager" --replicas=0
+## Post-init fixup
+
+# Ensure the controller manager will be able to access cluster configuration stored as
+# secrets in the federation namespace
+oadm --namespace "${FEDERATION_NAMESPACE}" policy add-role-to-user admin "${SERVICE_ACCOUNT}"
+
+# Scale the deployment down to avoid triggering an update after each patch
+${KC} scale deploy "${CONTROLLER_MANAGER}" --replicas=0
 
 # Disable the services controller to avoid a dependency on dnsaas
-${KC} patch deploy ${NAME}-controller-manager --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--controllers=services=false"}]'
+# TODO enable coredns when documentation is available
+${KC} patch deploy "${CONTROLLER_MANAGER}" --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--controllers=services=false"}]'
 
-## Binmount in hyperkube for development purposes
+## Mount a local hyperkube binary if present
+if [[ -f "${HYPERKUBE_PATH}" ]]; then
+  # Add volume for hyperkube
+  ${KC} patch deploy "${CONTROLLER_MANAGER}" --type=json -p='[{"op": "add", "path": "/spec/template/spec/volumes/-", "value": {"name": "hyperkube", "hostPath": {"path": "'"${HYPERKUBE_PATH}"'"}}}]'
 
-# # Add volume for hyperkube
-# ${KC} patch deploy ${NAME}-controller-manager --type=json -p='[{"op": "add", "path": "/spec/template/spec/volumes/0", "value": {"name": "hyperkube", "hostPath": {"path": "'"${HYPERKUBE_PATH}"'"}}}]'
+  # Add volume mount
+  ${KC} patch deploy "${CONTROLLER_MANAGER}" --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/volumeMounts/-", "value": {"mountPath": "/hyperkube", "name": "hyperkube", "readOnly": true}}]'
+fi
 
-# # Add volume mount
-# ${KC} patch deploy ${NAME}-controller-manager --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/volumeMounts/0", "value": {"mountPath": "/hyperkube", "name": "hyperkube", "readOnly": true}}]'
+# Scale the deployment back up
+${KC} scale deploy "${CONTROLLER_MANAGER}" --replicas=1
 
-# # Scale the deployment back up
-${KC} scale deploy "${NAME}-controller-manager" --replicas=1
+
