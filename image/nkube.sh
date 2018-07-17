9b7611c593b080159adc41dba69f28845e13cd42
Maru Newby <marun@redhat.com>
2018-07-17 09:57:21 +0000

    Fix node initialization

--- image/nkube.sh
+++ image/nkube.sh
@@ -36,11 +36,7 @@ function init-master() {
 
   local dns_name="${cluster_id}-nkube.${namespace}.svc.cluster.local"
 
-  # Ensure the kubelet will be compatible with running in a container.
-  echo 'KUBELET_EXTRA_ARGS=--cgroups-per-qos=false --enforce-node-allocatable= --fail-swap-on=false --v=4' > /etc/sysconfig/kubelet
-
-  # Cache images
-  kubeadm config images pull
+  common-setup
 
   # Initialize the cluster
   # TODO ensure different network cidrs than the hosting cluster
@@ -61,6 +57,14 @@ function init-master() {
   kubectl --kubeconfig="${config}" create -f /etc/nkube/calico.yaml
 }
 
+function common-setup() {
+  # Cache images
+  kubeadm config images pull
+
+  # Ensure the kubelet will be compatible with running in a container.
+  echo 'KUBELET_EXTRA_ARGS=--cgroups-per-qos=false --enforce-node-allocatable= --fail-swap-on=false --v=4' > /etc/sysconfig/kubelet
+}
+
 # TODO figure out how to compose the token in the template
 function get-kubeadm-token() {
   # Recent versions of kubeadm require a token with only lower-cased
@@ -77,19 +81,19 @@ function init-node() {
     return 0
   fi
 
-  # Cache images
-  kubeadm config images pull
+  common-setup
 
   local token; token="$(get-kubeadm-token)"
   local sa_dir="/var/run/secrets/kubernetes.io/serviceaccount"
   local namespace; namespace="$(cat ${sa_dir}/namespace)"
   local cluster_id; cluster_id="$(cat /etc/nkube/config/cluster-id)"
-  local dns_name="${cluster_id}-nkube.${namespace}.svc.cluster.local"
+  local dns_name="${cluster_id}-nkube.${namespace}.svc"
   local ip_addr; ip_addr="$(getent hosts "${dns_name}" | awk '{print $1}')"
 
   # TODO skip preflight checks for now because centos doesn't have the
   # 'configs' module available
-  while ! kubeadm join --ignore-preflight-errors=all --token="${token}" "${ip_addr}"; do
+  local join_cmd="kubeadm join ${ip_addr}:443 --ignore-preflight-errors=all --discovery-token-unsafe-skip-ca-verification --token=${token} "
+  while ! ${join_cmd}; do
     sleep 1
   done
 }
